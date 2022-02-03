79cb72b51fb3b15e39afc83b534e20e430b13b01
200101 <200101@e0601-19.JC-21.local>
2022-02-03 12:49:20 +0900

    Otokadai06を作成

---
+++ src/OtoKadai6.java
@@ -0,0 +1,44 @@
+import jp.jc21.t.yoshizawa.otopika.Otopika;
+
+/**
+ *
+ */
+
+
+/**
+ * @author AB2B12
+ *
+ */
+public class OtoKadai6  {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Otopika oto1 = new Otopika();
+		Otopika oto2 = new Otopika();
+		oto1.powerOn();
+		oto2.powerOn();
+		int a= 10;
+		while(true) {
+			oto2.lightRed();
+			oto1.lightGreen();
+			oto1.sleep(10);
+			oto1.lightYellow();
+			oto1.sleep(3);
+			oto1.lightRed();
+			oto1.sleep(1);
+			
+			oto1.lightRed();
+			oto2.lightGreen();
+			oto2.sleep(10);
+			oto2.lightYellow();
+			oto2.sleep(3);
+			oto2.lightRed();
+			oto2.sleep(1);
+			
+		}
+
+	}
+
+}
