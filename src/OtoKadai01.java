a9bb4f52f850ce84dd5f66859164d320f5dd6606
200101 <200101@e0601-19.JC-21.local>
2022-02-07 10:48:16 +0900

    Otokadai01を作成

--- src/OtoKadai01.java
+++ src/OtoKadai01.java
@@ -17,13 +17,12 @@ public class OtoKadai01  {
 	public static void main(String[] args) {
 		Otopika oto = new Otopika();
 		oto.powerOn();
+
 		while(true) {
 			oto.lightAll();
 			oto.sleep();
 			oto.lightOff();
 			oto.sleep();
 		}
-
 	}
-
 }
